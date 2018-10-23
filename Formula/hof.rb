a9f07c1f8b0d99611c3001ea82721496c7df6da9
verdverm <tony@hofstadter.io>
2018-10-23 16:48:19 -0600

    Brew formula update for hof version 0.1.7

--- Formula/hof.rb
+++ Formula/hof.rb
@@ -1,9 +1,9 @@
 class Hof < Formula
   desc "Hofstadter Studios CLI"
   homepage "https://github.com/hofstadter-io/hof"
-  url "https://github.com/hofstadter-io/hof/releases/download/0.1.6/hof_0.1.6_MacOS-64bit.tar.gz"
-  version "0.1.6"
-  sha256 "c140cda89240ae98dfcafc26f12897d968b7a95296073b5f0335b36e1800882a"
+  url "https://github.com/hofstadter-io/hof/releases/download/0.1.7/hof_0.1.7_MacOS-64bit.tar.gz"
+  version "0.1.7"
+  sha256 "29653aa8c5d762561d01dfcf8b0756cd50943e35d7632ca7c9c840225a56c05f"
 
   def install
     bin.install "hof"
